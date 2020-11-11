2db1c28eddab105aec2ed720c2ee6ec3bb466229
stephanie morrison <stephmorr.to@gmail.com>
2020-11-10 20:04:06 -0500

    changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const router = express.Router();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
